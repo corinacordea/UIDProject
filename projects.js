6b421cf2e79e4ec3fefa9dafad7137f8a07e5f3a
corinacordea <corina.cordea@yahoo.com>
2018-12-09 19:23:46 +0200

    Projects page

---
+++ projects.js
@@ -0,0 +1,10 @@
+window.onload = function (){
+	
+}
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(46.7712,23.6236),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
