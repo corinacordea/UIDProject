9e6489da0a08684ad0373e346ba31e954acc9297
Nicolae-Florian Onica <onicaflorin96@gmail.com>
2018-12-09 18:28:32 +0200

    Login+Register

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+$(document).ready(function() {
+  $('.input').blur(function() {
+    var $this = $(this);
+    if ($this.val())
+      $this.addClass('used');
+    else
+      $this.removeClass('used');
+  });
+});
 
+$('#tab1').on('click' , function(){
+    $('#tab1').addClass('login-shadow');
+   $('#tab2').removeClass('signup-shadow');
+});
+
+$('#tab2').on('click' , function(){
+    $('#tab2').addClass('signup-shadow');
+   $('#tab1').removeClass('login-shadow');
+});
