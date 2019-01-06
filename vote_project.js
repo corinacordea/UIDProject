f314cafd367e254ff113ef7c0742c118db31b755
birisalexandra <birisalexandra@gmail.com>
2019-01-06 15:26:11 +0200

    Vote button interaction

--- vote_project.js
+++ vote_project.js
@@ -1,4 +1,16 @@
 window.onload = function (){
 	document.getElementById("project_title").innerText = localStorage.projectTitle;
 	document.getElementById("project_image").setAttribute("src",localStorage.projectImage);
+	document.getElementById("submitButton").onclick = vote;
+}
+
+function vote() {
+	if (document.getElementById("submitButton").innerHTML == "Vote") {
+		document.getElementById("submitButton").innerHTML = "Voted";
+		alert("Your vote has been recorded. Thank you for your contribution!");
+	}
+	else {
+		alert("You already voted for this project!");
+	}
+	return false;
 }
