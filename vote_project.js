b0a99520c00e1a4734a12c0d735fa776836e0db7
birisalexandra <birisalexandra@gmail.com>
2019-01-09 21:25:52 +0200

    Add modals for some buttons

--- vote_project.js
+++ vote_project.js
@@ -7,7 +7,6 @@ window.onload = function (){
 function vote() {
 	if (document.getElementById("submitButton").innerHTML == "Vote") {
 		document.getElementById("submitButton").innerHTML = "Voted";
-		alert("Your vote has been recorded. Thank you for your contribution!");
 	}
 	else {
 		alert("You already voted for this project!");
