2ad1a1af0c5abedf15d5f94765334020aea7d4e5
birisalexandra <birisalexandra@gmail.com>
2019-01-09 23:55:43 +0200

    Added map

--- projects.js
+++ projects.js
@@ -44,11 +44,3 @@ function redirect(){
 	} 
   	return false;
 }
-
-function myMap() {
-var mapProp= {
-    center:new google.maps.LatLng(46.7712,23.6236),
-    zoom:5,
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
