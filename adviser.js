0138132082f916e248489fbff4b2afcacb48d7b8
corinacordea <corina.cordea@yahoo.com>
2019-01-12 23:26:42 +0200

    send info to review pages

--- adviser.js
+++ adviser.js
@@ -1,6 +1,11 @@
 window.onload = function (){
 	document.getElementById("project_title").innerText = localStorage.projectTitle;
 	document.getElementById("project_image").setAttribute("src",localStorage.projectImage);
+	document.getElementById("type").innerText = localStorage.projectType;
+	document.getElementById("category").innerText = localStorage.projectCategory;
+	document.getElementById("address").innerText = localStorage.projectAddress;
+	document.getElementById("description").innerText = localStorage.projectDescription;
+	document.getElementById("price").innerText = localStorage.projectPrice;
 	document.getElementById("updateButton").onclick = review;
 }
 
